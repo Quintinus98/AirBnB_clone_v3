e108e26f0dff155c6e6897f34fafa60fe3983cd9
David <obododavidsop@gmail.com>
2024-02-25 02:02:33 +0100

    stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,39 @@
 """ Apps Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review,
+           "states": State, "users": User}
 
 
 @app_views.route('/status')
 def status():
     """Returns a JSON"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def retrieve_endpoint():
+    """Retrieves endpoint"""
+    template = {
+        "amenities": 0,
+        "cities": 0,
+        "places": 0,
+        "reviews": 0,
+        "states": 0,
+        "users": 0
+    }
+    for key, value in classes.items():
+        total = storage.count(value)
+        template[key] += total
+
+    return jsonify(template)
