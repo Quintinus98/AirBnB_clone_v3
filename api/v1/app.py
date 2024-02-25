1d49c843990fc173e07af3cdf4d405bc27965d2e
David <obododavidsop@gmail.com>
2024-02-25 01:43:00 +0100

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,11 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
+
+
+HBNB_API_HOST = getenv('HBNB_API_HOST')
+HBNB_API_PORT = getenv('HBNB_API_PORT')
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -17,4 +22,8 @@ def teardown_storage(exception):
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', 5000, threaded=True, debug=True)
+    if not HBNB_API_HOST:
+        HBNB_API_HOST = '0.0.0.0'
+    if not HBNB_API_PORT:
+        HBNB_API_PORT = 5000
+    app.run(HBNB_API_HOST, HBNB_API_PORT, threaded=True, debug=True)
