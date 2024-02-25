23d9bbef345836faf2d1ea6bb40bbb00e365b830
David <obododavidsop@gmail.com>
2024-02-25 02:17:29 +0100

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -22,7 +22,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def retrieve_endpoint():
     """Retrieves endpoint"""
     template = {
