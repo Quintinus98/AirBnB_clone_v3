037327523481999392c8e21b056f4a7d0d75858d
David <obododavidsop@gmail.com>
2024-02-26 11:23:14 +0100

    updated storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -81,23 +81,16 @@ class DBStorage:
         """Retrieves one object"""
         if cls not in classes.values():
             return None
-
-        all_objs = models.storage.all(cls)
-        for value in all_objs.values():
+        all_instance = self.all(cls)
+        for value in all_instance.values():
             if (value.id == id):
                 return value
-
         return None
 
     def count(self, cls=None):
         """Returns the number of objects in storage"""
-        all_objs = classes.values()
-
-        if not cls:
-            count = 0
-            for ins in all_objs:
-                count += len(models.storage.all(ins).values())
-        else:
-            count = len(models.storage.all(cls).values())
-
-        return count
+        if cls is None:
+            return len(self.all())
+        if cls in classes.values():
+            return len(self.all(cls))
+        return (0)
