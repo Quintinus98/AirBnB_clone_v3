51ae183fa126e6f631ea28fdfe8334761980febc
David <obododavidsop@gmail.com>
2024-02-26 17:01:36 +0100

    uuid added

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,9 +2,10 @@
 """ States Views """
 from api.v1.views import app_views
 from flask import jsonify, abort, request
-from models.state import State
 from models import storage
-
+from models.state import State
+from datetime import datetime
+import uuid
 
 @app_views.route('/states/', methods=["GET"])
 def all_states():
