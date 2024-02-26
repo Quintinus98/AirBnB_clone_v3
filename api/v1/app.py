c3711afefffd51a68a3a7d0b5c9d2e42bce82c23
David <obododavidsop@gmail.com>
2024-02-27 00:25:28 +0100

    added cors header

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Apps API """
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -12,6 +13,7 @@ HBNB_API_PORT = getenv('HBNB_API_PORT')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
